23a5d86433951a7d60fdea9d9934447f6dfd0e6a
Miguel Jalon <migalonguerra@gmail.com>
2022-07-10 11:29:23 +0200

    update storageNFTs

--- contracts/BasicNFT.sol
+++ contracts/BasicNFT.sol
@@ -1,28 +1,30 @@
 // SPDX-License-Identifier: MIT
-import "@openzeppelin/contracts/ERC721/ERC721.sol"
+import "@openzeppelin/contracts/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 
-contract BasicNFT is ERC721 {
+contract BasicNFT is ERC721URIStorage, Ownable {
     string public constant TOKEN_URI = "";
     uint private tokenCounter;
 
     constructor() ERC721("Calliope", CALI) { 
         tokenCounter = 0;
 
-        function mintNFT() public returns(uint256){
-            _safeMint(msg.sender, tokenCounter);
+        function mintNFT() public onlyOwner returns(uint256){
             tokenCounter += 1;
+            _safeMint(msg.sender, tokenCounter);
             return tokenCounter;
         }
 
-        function tokenURI(uint256 /*tokenId*/) public view override returns (string memory) {
-            return TOKEN_URI;
-        }
+        // function tokenURI(uint256 /*tokenId*/) public view override returns (string memory) {
+        //     return TOKEN_URI;
+        // }
 
-        function getTokenCounter public view returns(uint256){
-            return tokenCounter;
-        }
+        // function getTokenCounter public view returns(uint256){
+        //     return tokenCounter;
+        // }
 
 
         
